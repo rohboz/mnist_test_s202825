3e352f493a3781d686e0e555e6ff76175b8cfa5f
rohboz <33161645+rohboz@users.noreply.github.com>
2022-01-18 13:13:39 +0100

    add config for train

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-# import os
-# _PROJECT_ROOT = os.path.dirname(__file__)  # root of project
-# _PATH_TEST = os.path.dirname(_PROJECT_ROOT, "Tests")  # root of test folder
-# _PATH_DATA = os.path.join(_PROJECT_ROOT, "Data")  # root of data
-# _PATH_MODEL = os.path.join(_PROJECT_ROOT, "Models")  # root of models
+import os
+_PROJECT_ROOT = os.path.dirname(__file__)  # root of project
+_PATH_TEST = os.path.dirname(_PROJECT_ROOT, "Tests")  # root of test folder
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "Data")  # root of data
+_PATH_MODEL = os.path.join(_PROJECT_ROOT, "Models")  # root of models
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,70 +4,77 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
+import hydra
 
-filepath = "data/processed/"
-outpath = "models/trained_models/"
-train = torch.load(filepath + "trainset.pt")
+root = os.getcwd()
 
-train_set, val_set = torch.utils.data.random_split(train, [24000, 1000])
-valloader = torch.utils.data.DataLoader(val_set, batch_size=1000, shuffle=True)
-trainloader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
+@hydra.main(config_name="conf_train_model.yaml")
+def main(cfg):
+    filepath = root+"\\data\\processed\\"
+    outpath = root+"\\models\\trained_models\\"
+    train = torch.load(filepath + "trainset.pt")
 
-model = nn.Sequential(
-    nn.Linear(784, 256),
-    nn.ReLU(),
-    nn.Dropout(0.25),
-    nn.Linear(256, 128),
-    nn.ReLU(),
-    nn.Dropout(0.25),
-    nn.Linear(128, 64),
-    nn.ReLU(),
-    nn.Dropout(0.25),
-    nn.Linear(64, 10),
-    nn.LogSoftmax(dim=1),
-)
+    train_set, val_set = torch.utils.data.random_split(train, [24000, 1000])
+    valloader = torch.utils.data.DataLoader(val_set, batch_size=1000, shuffle=True)
+    trainloader = torch.utils.data.DataLoader(train_set, batch_size=cfg.hyperparameters.batch_size, shuffle=True)
 
-criterion = nn.NLLLoss()
-optimizer = optim.Adam(model.parameters())
-epochs = 20
-Trn_loss = []
-Val_loss = []
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
+    model = nn.Sequential(
+        nn.Linear(784, 256),
+        nn.ReLU(),
+        nn.Dropout(0.25),
+        nn.Linear(256, 128),
+        nn.ReLU(),
+        nn.Dropout(0.25),
+        nn.Linear(128, 64),
+        nn.ReLU(),
+        nn.Dropout(0.25),
+        nn.Linear(64, 10),
+        nn.LogSoftmax(dim=1),
+    )
 
-        # TODO: Training pass
-        optimizer.zero_grad()
-        output = model(images.float())
-        loss = criterion(output, labels)
-        loss.backward()
-        optimizer.step()
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), cfg.hyperparameters.lr)
+    epochs = cfg.hyperparameters.epochs
+    Trn_loss = []
+    Val_loss = []
+    for e in range(epochs):
+        running_loss = 0
+        for images, labels in trainloader:
+            # Flatten MNIST images into a 784 long vector
+            images = images.view(images.shape[0], -1)
 
-        running_loss += loss.item()
-    else:
-        Trn_loss.append(running_loss / len(trainloader))
-        print(f"Training loss: {Trn_loss[-1]}")
-        # Flatten MNIST images into a 784 long vector
-        images, labels = next(iter(valloader))
-        images = images.view(images.shape[0], -1)
-        # TODO: Validation pass
-        optimizer.zero_grad()
-        output = model(images.float())
-        loss = criterion(output, labels)
-        Val_loss.append(loss.item())
-        print(f"Validation loss: {Val_loss[-1]}")
+            # TODO: Training pass
+            optimizer.zero_grad()
+            output = model(images.float())
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
 
+            running_loss += loss.item()
+        else:
+            Trn_loss.append(running_loss / len(trainloader))
+            print(f"Training loss: {Trn_loss[-1]}")
+            # Flatten MNIST images into a 784 long vector
+            images, labels = next(iter(valloader))
+            images = images.view(images.shape[0], -1)
+            # TODO: Validation pass
+            optimizer.zero_grad()
+            output = model(images.float())
+            loss = criterion(output, labels)
+            Val_loss.append(loss.item())
+            print(f"Validation loss: {Val_loss[-1]}")
+            
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+    X = range(len(Trn_loss))
+    plt.plot(X, Trn_loss)
+    plt.plot(X, Val_loss)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend(["Training Loss", "Validation Loss"])
+    plt.savefig(root+"\\reports\\figures\\model.png")
 
-X = range(len(Trn_loss))
-plt.plot(X, Trn_loss)
-plt.plot(X, Val_loss)
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
-plt.legend(["Training Loss", "Validation Loss"])
-plt.savefig("reports/figures/model.png")
+    torch.save(model.state_dict(), outpath + "model.pt")
 
-torch.save(model.state_dict(), outpath + "model.pt")
+if __name__ == "__main__":
+    main()
